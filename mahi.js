20274df57b74c8607ed212436857ab3406d675e8
MahendarThota <139948377+MahendarThota@users.noreply.github.com>
2024-02-20 10:53:36 +0530

    Add files via upload

---
+++ mahi.js
@@ -0,0 +1,10 @@
+let clickButtonEl=document.getElementById("clickButton") 
+let bgcontainer=document.getElementById("bg-container")
+let card=document.getElementById("card")
+function clickButton(){
+    bgcontainer.style.display="none"
+    card.style.display="block"
+    
+  
+    
+}
